02d857cb33f33788f1801a27388809df81a626a0
GB <goncalosetubal12@gmail.com>
2023-05-09 15:51:15 +0100

    add 3c and changed 3a

--- Exercise3.py
+++ Exercise3.py
@@ -1,10 +1,14 @@
 import math
 import os
 import random
+import string
 
 
-def symbol_generator(symbols, size, probabilities, file_name):
-    output_array = random.choices(symbols, probabilities, k=size)
+def symbol_generator(symbols, size, file_name, probabilities=None):
+    if probabilities is None:
+        output_array = random.choices(symbols, k=size)
+    else:
+        output_array = random.choices(symbols, probabilities, k=size)
     f = open(file_name, "w")
     f.write(" ".join(output_array))
     f.close()
@@ -16,8 +20,8 @@ def entropy_compare(sizeM, sizeN):
     symbols = ['a', 'b', 'c', 'd', 'e', 'f']
     probabilities = [0.9, 0.1, 0, 0, 0, 0]
     entropy = 0
-    result1 = symbol_generator(symbols, sizeM, probabilities, "entropyCompare1").split(" ")
-    result2 = symbol_generator(symbols, sizeN, probabilities, "entropyCompare2").split(" ")
+    result1 = symbol_generator(symbols, sizeM, "entropyCompare1", probabilities).split(" ")
+    result2 = symbol_generator(symbols, sizeN, "entropyCompare2", probabilities).split(" ")
     entropy1 = entropy_calculator(symbols, result1)
     entropy2 = entropy_calculator(symbols, result2)
     return entropy1, entropy2
@@ -33,17 +37,27 @@ def entropy_calculator(symbols, text):
     return entropy
 
 
+def password_generator():
+    alphabet = string.ascii_letters + string.digits + string.punctuation
+    password_length = random.randint(8, 12)
+    return symbol_generator(alphabet, password_length, "passwords")
+
+
 def main_exercise_3():
     fmp = [0.9, 0.1, 0, 0, 0, 0]
     m = ['a', 'b', 'c', 'd', 'e', 'f']
     file_name = "symbolGenerator"
     print("result from generating symbols")
-    print(symbol_generator(m, 20, fmp, file_name))
+    print(symbol_generator(m, 20, file_name, fmp))
     open(file_name, "w").close()
     # comment this vvv if you want file to appear
     os.remove(file_name)
     print()
     print(entropy_compare(20, 100))
+    print()
+    print("spaces for better look")
+    for i in range(5):
+        print(password_generator())
 
 
 if __name__ == '__main__':
