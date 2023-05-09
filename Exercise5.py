cddaf3d17391881b264fac4980d769f617d7e29c
j-vie <j-viegas@sapo.pt>
2023-05-09 16:32:55 +0100

    Interferencia meio feita ???

--- Exercise5.py
+++ Exercise5.py
@@ -1,3 +1,5 @@
+
+import random as rand
 def binarySymmetricChannel(sequence, expectedBer):
     with open(sequence, 'r', encoding="utf-8") as file:
         char = file.read(1)
@@ -8,9 +10,38 @@ def binarySymmetricChannel(sequence, expectedBer):
             if not char:
                 break
 
+def toBinary(text):
+    return ' '.join(format(ord(x), 'b') for x in text)
+
+
+def BSC(binary, prob):
+    print(rand.random())
+    interferencia = ""
+    print(binary)
+    for x in binary:
+        print("X => "+x)
+        if x == " ":
+            interferencia += " "
+            continue
+        is_wrong = rand.random() < prob
+        bit = x
+        if is_wrong:
+            bit = invertValue(bit)
+            interferencia += str(bit)
+        else:
+            interferencia += str(bit)
+    return interferencia
+
+def invertValue(x):
+    if x == 1:
+        return 0
+    return 1
 
 def main():
     binarySymmetricChannel("./TestFilesCD/a.txt", 1)
+    file = "hello world"
+    print(BSC(toBinary(file), 0.1))
+    print(toBinary(file))
 
 
 if __name__ == "__main__":
